93fdc65145260e41c4958b7a99ff1da493621a7f
smoore-a <smoore-a@student.42malaga.com>
2023-12-06 15:56:42 +0100

    modified calloc

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:10:11 by smoore-a          #+#    #+#             */
-/*   Updated: 2023/12/06 15:54:55 by smoore-a         ###   ########.fr       */
+/*   Updated: 2023/12/06 15:56:17 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ void	*ft_calloc(size_t count, size_t size)
 	full_size = count * size;
 	ptr = (unsigned char *) malloc(full_size * sizeof(char));
 	if (ptr == NULL)
-	{
 		return (0);
-	}
 	ft_bzero(ptr, full_size);
 	return (ptr);
 }
