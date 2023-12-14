c5d6f2bbb3c39a13e56583d67f2a20c4a31b07fa
seilanmoore <seilanmoore42@gmail.com>
2023-12-14 21:44:01 +0100

    modified itoa

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:40:52 by smoore-a          #+#    #+#             */
-/*   Updated: 2023/12/12 17:50:56 by smoore-a         ###   ########.fr       */
+/*   Updated: 2023/12/14 21:39:52 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,41 @@ static size_t	count_digit(int n)
 	return (len);
 }
 
+static	char	*int_min(void)
+{
+	char	*nbr;
+
+	nbr = ft_strdup("-2147483648");
+	if (!nbr)
+		return (NULL);
+	return (nbr);
+}
+
 char	*ft_itoa(int n)
 {
+	int		sign;
 	size_t	len;
 	char	*nbr;
 
+	sign = 0;
 	len = count_digit(n);
+	if (n == -2147483648)
+		return (int_min());
 	nbr = (char *)malloc((len + 1) * sizeof(char));
 	if (!nbr)
 		return (NULL);
-	nbr[len--] = '\0';
-	if (n == 0)
-	{
-		*nbr = '0';
-		return (nbr);
-	}
+	nbr[len] = '\0';
 	if (n < 0)
-		*nbr = '-';
-	while (n != 0)
 	{
-		if (*nbr == '-')
-			nbr[len--] = '0' + -(n % 10);
-		else
-			nbr[len--] = '0' + (n % 10);
+		n = -n;
+		sign = 1;
+	}
+	while (len-- > 0)
+	{
+		nbr[len] = '0' + (n % 10);
 		n /= 10;
 	}
+	if (sign)
+		nbr[0] = '-';
 	return (nbr);
 }
