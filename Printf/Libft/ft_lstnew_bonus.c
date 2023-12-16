09f1092cd90af1ed687211cbc141af11ef72dde7
seilanmoore <seilanmoore42@gmail.com>
2023-12-16 17:54:36 +0100

    added printf

---
+++ Printf/Libft/ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 11:42:40 by smoore-a          #+#    #+#             */
+/*   Updated: 2023/12/13 15:19:26 by smoore-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
---
+++ Printf/Libft/ft_strtrim.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 16:04:59 by smoore-a          #+#    #+#             */
+/*   Updated: 2023/12/11 13:18:52 by smoore-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	const char	*start;
+	const char	*end;
+	char		*trimmed;
+	size_t		len;
+
+	start = s1;
+	end = s1 + ft_strlen(s1);
+	while (*start && ft_strchr(set, *start))
+		start++;
+	while (end > start && ft_strchr(set, *(end - 1)))
+		end--;
+	len = end - start;
+	trimmed = malloc(len + 1);
+	if (!trimmed)
+		return (NULL);
+	ft_strlcpy(trimmed, start, len + 1);
+	return (trimmed);
+}
