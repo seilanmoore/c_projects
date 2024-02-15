4f4190e8806897fba6235d0fac492dcac0ae229d
seilanmoore <seilanmoore42@gmail.com>
2024-02-15 14:53:57 +0100

    fix

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,30 +6,11 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 17:22:59 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/02/15 13:54:32 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/02/15 14:51:58 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-
-void	last_short(t_stack **stack_a)
-{
-	size_t	min_a;
-	size_t	half_a;
-
-	min_a = get_min(*stack_a);
-	half_a = lst_size(*stack_a) / 2;
-	if (min_a > half_a)
-	{
-		while ((*stack_a)->index != min_a)
-			rotate_a(&(*stack_a), 0);
-	}
-	else
-	{
-		while ((*stack_a)->index != min_a)
-			reverse_rotate_a(&(*stack_a), 0);
-	}
-}
 
 int	main(int argc, char	**argv)
 {
