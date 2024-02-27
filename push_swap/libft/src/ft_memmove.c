557258cf9a5c41ba3f72c45675438dde2adf9684
seilanmoore <seilanmoore42@gmail.com>
2024-02-27 11:45:18 +0100

    fixes

---
+++ push_swap/libft/src/ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 13:10:25 by smoore-a          #+#    #+#             */
+/*   Updated: 2024/02/27 11:43:39 by smoore-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char		*ptr_dst;
+	const char	*ptr_src;
+
+	if (dst > src)
+	{
+		ptr_dst = dst + len;
+		ptr_src = src + len;
+		while (len-- > 0)
+			*(--ptr_dst) = *(--ptr_src);
+	}
+	else
+		ft_memcpy(dst, src, len);
+	return (dst);
+}
---
+++ push_swap/libft/src/ft_strchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 13:33:46 by smoore-a          #+#    #+#             */
+/*   Updated: 2024/02/27 11:43:39 by smoore-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+#include "../../include/libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
