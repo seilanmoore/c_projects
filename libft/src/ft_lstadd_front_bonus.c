2f1f74e524a653067b62eaa703a33da0f797b59a
seilanmoore <seilanmoore42@gmail.com>
2024-05-31 12:10:23 +0200

    rm line in ft_lstadd_front

--- libft/src/ft_lstadd_front_bonus.c
+++ libft/src/ft_lstadd_front_bonus.c
@@ -6,11 +6,12 @@
 /*   By: smoore-a <smoore-a@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:25:09 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/05/23 19:20:24 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/05/26 12:37:51 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (lst && new)
