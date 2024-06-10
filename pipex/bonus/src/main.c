7f15842b33183d0bcd5c0e0f931a8e0384b8ec0f
seilanmoore <seilanmoore42@gmail.com>
2024-06-10 10:39:21 +0200

    backup

--- pipex/bonus/src/main.c
+++ pipex/bonus/src/main.c
@@ -6,7 +6,7 @@
 /*   By: smoore-a <smoore-a@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 22:13:08 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/06/10 10:16:23 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/06/10 10:37:49 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,22 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
+	char	*line;
+	char 	*limit;
 
 	init(&data, argc, argv, envp);
+	limit = ft_strjoin(data.argv[2], "\n");
+	line = get_next_line(0);
+	while (line && ft_strncmp(line, limit, ft_strlen(line)))
+	{
+		ft_putstr_fd(line, data.file[1].fd);
+		free(line);
+		line = get_next_line(0);
+	}
+	if (line)
+		free(line);
+	if (limit)
+		free(limit);
 	cleanup(&data);
 	exit(EXIT_SUCCESS);
 }
