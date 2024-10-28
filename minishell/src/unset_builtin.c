f2d6f8fbaceb34517d0855e20cb6cb5493eccc9b
Seilan <seilanmoore@gmail.com>
2024-10-28 13:47:28 +0100

    del_env fixed.2

--- minishell/src/unset_builtin.c
+++ minishell/src/unset_builtin.c
@@ -6,7 +6,7 @@
 /*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 10:51:43 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/10/28 13:32:53 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:45:20 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 int	unset_builtin(t_data *data, t_cmd *cmd)
 {
 	char	**arg;
+	int		update;
 	int		i;
 
 	arg = cmd->args;
+	update = 0;
 	i = 0;
 	while (arg[++i])
 	{
 		if (get_env_var(data->env, arg[i]))
+		{
 			del_env(&(data->env), arg[i]);
+			update = 1;
+		}
 		if (get_env_var(data->locals, arg[i]))
 			del_env(&(data->locals), arg[i]);
-		upd_env(data);
 	}
+	if (update)
+		upd_env(data);
 	return (0);
 }
