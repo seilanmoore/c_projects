27a15d7c1e12c74c4dd86b42537284d240df6644
Seilan <seilanmoore@gmail.com>
2024-11-20 10:25:11 +0100

    philo fix

--- philo/src/utils/error.c
+++ philo/src/utils/error.c
@@ -3,26 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoore-a <smoore-a@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: smoore-a <smoore-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 19:00:05 by smoore-a          #+#    #+#             */
-/*   Updated: 2024/06/18 09:34:15 by smoore-a         ###   ########.fr       */
+/*   Updated: 2024/11/20 10:22:31 by smoore-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
+#include <unistd.h>
 
 int	ft_error(void *data, char *message, void *aux)
 {
 	if (message)
 	{
-		write(2, message, ft_strlen(message));
+		write(STDOUT_FILENO, message, ft_strlen(message));
 		if (aux)
 		{
-			write(2, ": ", 2);
-			write(2, aux, ft_strlen(aux));
+			write(STDOUT_FILENO, ": ", 2);
+			write(STDOUT_FILENO, aux, ft_strlen(aux));
 		}
-		write(2, "\n", 1);
+		write(STDOUT_FILENO, "\n", 1);
 	}
 	ft_clear(data);
 	return (EXIT_FAILURE);
