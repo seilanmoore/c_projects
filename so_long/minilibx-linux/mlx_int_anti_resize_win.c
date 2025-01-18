76b5e83348827b04eeb88d12f33938bcea53bfc5
Seilan <seilanmoore@gmail.com>
2025-01-18 12:57:34 +0100

    ft_calloc fix; minilibx fix

--- so_long/minilibx-linux/mlx_int_anti_resize_win.c
+++ so_long/minilibx-linux/mlx_int_anti_resize_win.c
@@ -1,9 +1,9 @@
 /*
-** mlx_int_anti_resize_win.c for MiniLibX in 
-** 
+** mlx_int_anti_resize_win.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Tue Aug  8 14:31:05 2000 Charlie Root
 ** Last update Tue Sep 25 15:56:58 2001 Charlie Root
 */
@@ -15,8 +15,14 @@ int	mlx_int_anti_resize_win(t_xvar *xvar,Window win,int w,int h)
 {
   XSizeHints    hints;
   long		toto;
-  
-  XGetWMNormalHints(xvar->display,win,&hints,&toto);
+
+  hints = (XSizeHints){0};
+  toto = 0;
+  if (!XGetWMNormalHints(xvar->display,win,&hints,&toto))
+  {
+    hints = (XSizeHints){0};
+    toto = 0;
+  }
   hints.width = w;
   hints.height = h;
   hints.min_width = w;
